1fff9e482b28e44a4a935378e40ba40a263eb5d1
Leoton H Silva <leoton.silva@gmail.com>
2022-01-22 18:10:16 -0300

    usando dentro de uma fnão

--- aula29_Switch_Case/index.js
+++ aula29_Switch_Case/index.js
@@ -1,54 +1,37 @@
-const data = new Date('1987-04-26 00:00:00');
-let diaSemana = data.getDay();
-// diaSeemana = 7;
-let diaSemanaTexto;
+function getDayWeekText(dayWeek) {
+    let dayWeekText
 
-switch (diaSemana) {
-    case 0:
-        diaSemanaTexto = 'Domingo';
-        break;
-    case 1:
-        diaSemanaTexto = 'Segunda';
-        break;
-    case 2:
-        diaSemanaTexto = 'Terça';
-        break;
-    case 3:
-        diaSemanaTexto = 'Quarta';
-        break;
-    case 4:
-        diaSemanaTexto = 'Quinta';
-        break;
-    case 5:
-        diaSemanaTexto = 'Sexta';
-        break;
-    case 6:
-        diaSemanaTexto = 'Sábado';
-        break;
-    default:
-        diaSemanaTexto = '';
-        break;
-
+    switch (dayWeek) {
+        case 0:
+            dayWeekText = 'Domingo';
+            return dayWeekText;
+        case 1:
+            dayWeekText = 'Segunda';
+            return dayWeekText;;
+        case 2:
+            dayWeekText = 'Terça';
+            return dayWeekText;;
+        case 3:
+            dayWeekText = 'Quarta';
+            return dayWeekText;;
+        case 4:
+            dayWeekText = 'Quinta';
+            return dayWeekText;;
+        case 5:
+            dayWeekText = 'Sexta';
+            return dayWeekText;;
+        case 6:
+            dayWeekText = 'Sábado';
+            return dayWeekText;;
+        default:
+            dayWeekText = '';
+            return dayWeekText;;
+    
+    }
 }
 
+const data = new Date('1987-04-31 00:00:00');
+let dayWeek = data.getDay();
+let dayWeekText = getDayWeekText(dayWeek);
 
-
-// if (diaSemana === 0) {
-//     diaSemanaTexto = 'Domingo';
-// } else if (diaSemana === 1) {
-//     diaSemanaTexto = 'Segunda';
-// } else if (diaSemana === 2) {
-//     diaSemanaTexto = 'terça';
-// } else if (diaSemana === 3) {
-//     diaSemanaTexto = 'quarta';
-// } else if (diaSemana === 4) {
-//     diaSemanaTexto = 'quinta';
-// } else if (diaSemana === 5) {
-//     diaSemanaTexto = 'sexta';
-// } else if (diaSemana === 6) {
-//     diaSemanaTexto = 'sábado';
-// } else {
-//     diaSemanaTexto = '';
-// }
-
-console.log(diaSemana, diaSemanaTexto);
+console.log(dayWeek, dayWeekText);

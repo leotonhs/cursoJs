2b878c29ee3c61b761337b49e56bf84a1b34827e
Leoton H Silva <leoton.silva@gmail.com>
2022-01-25 17:22:10 -0300

    retornar maior numero, math e exemplo com arrow function

---
+++ aula41_Exercício_com_lógica_de_programação_01/index.js
@@ -0,0 +1,14 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+// const num1 = 52345234;
+// const num2 = 3452345234;
+
+// function numMax (num1, num2){
+//     const max = Math.max(num1, num2)
+//     return max;
+// }
+// console.log(numMax(num1, num2));
+
+//USANDO ARROW FUNCTION
+
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(10, 5));

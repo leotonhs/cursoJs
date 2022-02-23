d70a903ab2465bed4dfbc23fc66fc22deeee3d9d
Leoton H Silva <leoton.silva@gmail.com>
2022-02-22 23:06:19 -0300

    Método splice

---
+++ aula63_Metodo_splice/index.js
@@ -0,0 +1,12 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia'];
+
+// Unshift simulado
+nomes.splice(2, 0, 'Luiz', 'Otávio');
+console.log(nomes);
+
+// nomes.splice(índice atual, delete, elem1, elem2, elem3);
+// pop simulado
+// const removidos = nomes.splice(-1,1);
+
+// shift simulado
+// const removidos = nomes.splice(0, 1); 

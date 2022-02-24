ff9dba1129fde9261cca81c733ee98d474abc15f
Leoton H Silva <leoton.silva@gmail.com>
2022-02-24 19:52:59 -0300

    Simples comentários

---
+++ aula73_Métodos_úteis_para_objetos/index.js
@@ -0,0 +1,13 @@
+/*
+Object.values
+Object.entries
+Object.assign(des. any)
+Object.getOwnPropertyDescriptor(o, 'prop')
+... (spread)
+
+// Já vimos
+Object.keys (retorna as chaves)
+Object.freeze (congela o objeto)
+Object.defineProperties (define várias propriedades)
+Object.defineProperty (define uma propriedade)
+*/
